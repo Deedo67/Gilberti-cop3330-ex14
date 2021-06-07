0c6dfaa3683883fd15bf69f632ef88446f8fe20c
Deedo67 <70287389+Deedo67@users.noreply.github.com>
2021-06-06 23:10:59 -0400

    Initial commit

---
+++ src/main/java/MPE14/example/ex14.java
@@ -0,0 +1,24 @@
+package MPE14.example;
+
+import java.util.Scanner;
+
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Derek Gilberti
+ */
+
+//class begin
+public class ex14 {
+    //main method begin
+    public static void main(String[] args) {
+        //scanner
+        Scanner in = new Scanner(System.in);
+
+        //input
+        System.out.print("What is the order amount? ");
+        String strorder = in.nextLine();
+        System.out.print("What is the state? ");
+        String state = in.nextLine();
+
+    }
+}
